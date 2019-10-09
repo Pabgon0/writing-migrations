510205150509a20b07bb9926bcf173fa719bb856
Pablo Gonzalez <pabgon0@gmail.com>
2019-10-09 19:03:49 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,6 +1,8 @@
 class AddGradeAndBirthdateToStudents < ActiveRecord::Migration[5.2]
   def change
-    add_column :grade, :birthdate
+    add_column 
+    t.string :grade
+    t.integer :birthdate
   end
 
 end
